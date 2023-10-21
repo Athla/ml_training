7572d3801821e63e424121c969ee94e276cbdcfb
guilherme.grimm <guilher.c.rodrigues@gmail.com>
2023-10-21 09:50:08 -0300

    feat. primary model, unoptimized.

--- main.py
+++ main.py
@@ -6,22 +6,27 @@ from keras.layers import Dense
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
 import numpy as np
-def train_model():
-    data = pd.read_csv('src/predictive_maintenance.csv')
+from imblearn.under_sampling import NearMiss
+def train_model(model_name):
+    data = pd.read_csv('output/gen_data_20231008.csv')
 
-    new_labels = ['uid', 'prod_id', 'prod_type', 'air_temp', 'process_temp', 'rot_speed', 'torque', 'tool_wear', 'target', 'failure_type']
+    # new_labels = ['uid', 'prod_id', 'prod_type', 'air_temp', 'process_temp', 'rot_speed', 'torque', 'tool_wear', 'target', 'failure_type']
 
-    data.columns = new_labels
+    # data.columns = new_labels
     X = data[['torque','process_temp','tool_wear', "air_temp", "rot_speed"]]
-    y = data['target']
-
+    y = data['failure']
+    # y.dropna(inplace=True)
 
     # Dividindo os dados em treino e teste
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=123)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
     # Visualizando o shape dos dados
 
-    sm= SMOTE(random_state=2)
+#    nr= NearMiss()
+    
+#    X_train, y_train = nr.fit_resample(X_train, y_train)
+    # print(f"Depois da redução de dimensionalidade -> Falha: {sum(y_train ==1)}")
+    # print(f"Depois da redução de dimensionalidade -> Não falha: {sum(y_train == 0)}")
     model = Sequential([
         # Input layer
         Dense(units=5, input_shape=(5,), activation='relu', name='input_layer', ),
@@ -35,40 +40,43 @@ def train_model():
 
     model.compile(optimizer='adam', loss= 'sparse_categorical_crossentropy', metrics=['accuracy'])
 
-    # model.summary()
-
-    model.fit(X_train, y_train, epochs=150, batch_size=16, validation_split=0.2, verbose=2)
+    model.summary()
+    """Testar diferente quantidades de épocas, batchsize e validation split
+        Camada do meio com 10, 25 ou 50 neurônios
+        Testar com mais neurõnios
+    """
+    model.fit(X_train, y_train, epochs=50, batch_size=50, validation_split=0.2, verbose=2)
 
     loss, acc = model.evaluate(X_test, y_test, verbose=2)
 
     print(f"Accuracy: {acc*100:.2f}%, Loss: {loss*100:.2f}%")
 
-    model.save('src/model.h5')
+    model.save(f'src/{model_name}.keras')
     return model
 
-def use_model():
+def use_model(model_name:str):
     """
     Function to feed in real time data to the model. Data is found at output folder
     """
     try:
-        model = keras.models.load_model('src/model.h5')
+        model = keras.models.load_model(rf'src/{model_name}.keras')
     except OSError:
         model = train_model()
-        model = keras.models.load_model('src/model.h5')
+        model = keras.models.load_model(rf"src/{model_name}.keras")
     finally:
         data = pd.read_csv('output/realtime_data.csv')
         data = data[['torque','process_temp','tool_wear', "air_temp", "rot_speed"]]
         predictions = model.predict(data)
         predicted_class = np.argmax(predictions, axis=1)
-        predicted_class = np.argmax(predictions, axis=1)
-        
+        failed_machines = list()
+
         # Print predictions with indication of failure
         for i, prediction in enumerate(predicted_class):
             if prediction == 1:
-                print(f"Machine {i + 1}: Predicted Failure")
-            else:
-                continue
+                failed_machines.append(i+1)            
 
 
+        print(f"A total of {len(failed_machines)} have failed.")
 if __name__ == "__main__":
-    use_model()
+	train_model("complete_model_v2")
+	use_model("complete_model_v2")
